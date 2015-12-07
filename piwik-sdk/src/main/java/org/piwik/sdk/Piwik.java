3d0166afa60313f809b42637bd13b13def056642
dx8719 <dx8719@live.com>
2015-12-07 13:08:24 +0800

    Update Piwik.java
    修改eoc

--- piwik-sdk/src/main/java/org/piwik/sdk/Piwik.java
+++ piwik-sdk/src/main/java/org/piwik/sdk/Piwik.java
@@ -1,40 +1,40 @@
 /*
- * Android SDK for Piwik
+ * Android SDK for Eoc
  *
- * @link https://github.com/piwik/piwik-android-sdk
- * @license https://github.com/piwik/piwik-sdk-android/blob/master/LICENSE BSD-3 Clause
+ * @link https://github.com/eoc/eoc-android-sdk
+ * @license https://github.com/eoc/eoc-sdk-android/blob/master/LICENSE BSD-3 Clause
  */
 
-package org.piwik.sdk;
+package org.eoc.sdk;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 
-import org.piwik.sdk.tools.Logy;
+import org.eoc.sdk.tools.Logy;
 
 import java.net.MalformedURLException;
 
 
-public class Piwik {
+public class Eoc {
     public static final String LOGGER_PREFIX = "PIWIK:";
-    public static final String PREFERENCE_FILE_NAME = "org.piwik.sdk";
-    public static final String PREFERENCE_KEY_OPTOUT = "piwik.optout";
+    public static final String PREFERENCE_FILE_NAME = "org.eoc.sdk";
+    public static final String PREFERENCE_KEY_OPTOUT = "eoc.optout";
     private final Context mContext;
     private boolean mOptOut = false;
     private boolean mDryRun = false;
 
-    private static Piwik sInstance;
+    private static Eoc sInstance;
     private boolean mDebug = BuildConfig.DEBUG;
     private final SharedPreferences mSharedPreferences;
 
-    public static synchronized Piwik getInstance(Context context) {
+    public static synchronized Eoc getInstance(Context context) {
         if (sInstance == null)
-            sInstance = new Piwik(context);
+            sInstance = new Eoc(context);
         return sInstance;
     }
 
-    private Piwik(Context context) {
+    private Eoc(Context context) {
         mContext = context.getApplicationContext();
         mSharedPreferences = getContext().getSharedPreferences(PREFERENCE_FILE_NAME, Context.MODE_PRIVATE);
         mOptOut = getSharedPreferences().getBoolean(PREFERENCE_KEY_OPTOUT, false);
@@ -45,7 +45,7 @@ public class Piwik {
     }
 
     /**
-     * @param trackerUrl (required) Tracking HTTP API endpoint, for example, http://your-piwik-domain.tld/piwik.php
+     * @param trackerUrl (required) Tracking HTTP API endpoint, for example, http://your-eoc-domain.tld/eoc.php
      * @param siteId     (required) id of site
      * @param authToken  (optional) could be null or valid auth token.
      * @return Tracker object
@@ -58,7 +58,7 @@ public class Piwik {
     }
 
     /**
-     * @param trackerUrl (required) Tracking HTTP API endpoint, for example, http://your-piwik-domain.tld/piwik.php
+     * @param trackerUrl (required) Tracking HTTP API endpoint, for example, http://your-eoc-domain.tld/eoc.php
      * @param siteId     (required) id of site
      * @return Tracker object
      * @throws MalformedURLException
@@ -68,8 +68,8 @@ public class Piwik {
     }
 
     /**
-     * Use this to disable Piwik, e.g. if the user opted out of tracking.
-     * Piwik will persist the choice and remain disable on next instance creation.</p>
+     * Use this to disable Eoc, e.g. if the user opted out of tracking.
+     * Eoc will persist the choice and remain disable on next instance creation.</p>
      * The choice is stored in {@link #PREFERENCE_FILE_NAME} under the key {@link #PREFERENCE_KEY_OPTOUT}.
      *
      * @param optOut true to disable reporting
@@ -80,7 +80,7 @@ public class Piwik {
     }
 
     /**
-     * @return true if Piwik is currently disabled
+     * @return true if Eoc is currently disabled
      */
     public boolean isOptOut() {
         return mOptOut;
@@ -100,11 +100,11 @@ public class Piwik {
     }
 
     /**
-     * The dryRun flag set to true prevents any data from being sent to Piwik.
+     * The dryRun flag set to true prevents any data from being sent to Eoc.
      * The dryRun flag should be set whenever you are testing or debugging an implementation and do not want
-     * test data to appear in your Piwik reports. To set the dry run flag, use:
+     * test data to appear in your Eoc reports. To set the dry run flag, use:
      *
-     * @param dryRun true if you don't want to send any data to piwik
+     * @param dryRun true if you don't want to send any data to eoc
      */
     public void setDryRun(boolean dryRun) {
         mDryRun = dryRun;
@@ -115,9 +115,9 @@ public class Piwik {
     }
 
     /**
-     * Returns the shared preferences used by Piwik that are stored under {@link #PREFERENCE_FILE_NAME}
+     * Returns the shared preferences used by Eoc that are stored under {@link #PREFERENCE_FILE_NAME}
      *
-     * @return Piwik's SharedPreferences instance
+     * @return Eoc's SharedPreferences instance
      */
     public SharedPreferences getSharedPreferences() {
         return mSharedPreferences;
